eda761362392d120ec22fa81115a1b4d5c55be7b
novakw267 <novakw267@gmail.com>
2017-03-30 00:17:21 -0400

    Delete button works on the front end
    I got the delete button to work
    It will currently delete a blog without comments
    The only blog I can not delete is the one I added a comment to on the
    back end.
    I put delete in the blog-list/index.js
    and I changed the structure of the code in the templates blogs/index.hbs

--- app/components/blog-list/index.js
+++ app/components/blog-list/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete() {
+      console.log('almost');
+      this.sendAction('delete', this.get('blog'));
+    },
+  }
 });
