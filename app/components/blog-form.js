9f526d2fc7d6f9bbb89e0b79d49643f44494b700
novakw267 <novakw267@gmail.com>
2017-03-30 00:37:48 -0400

    Starting the edit button,
    I am setting up for the edit button and the form fields
    I think I am headed in the right direction
    I am using blog-list/post to structure the fields
    and blogs/edit.hbs to set the action path

--- app/components/blog-form.js
+++ app/components/blog-form.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    edit() {
-      console.log('edit');
-      this.sendAction('edit', this.get('blog'));
-    },
-    delete() {
-      console.log('almost');
-      this.sendAction('delete', this.get('blog'));
-    },
     save() {
       console.log('save', this.get('blog'));
       this.sendAction('save', this.get('blog'));
--- app/components/blog-list/index.js
+++ app/components/blog-list/index.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    edit() {
+      console.log('edit');
+
+      this.sendAction('edit', this.get('blog'));
+    },
     delete() {
       console.log('almost');
       this.sendAction('delete', this.get('blog'));
